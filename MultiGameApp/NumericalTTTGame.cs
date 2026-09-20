ce094d619ea7f02a2ec8a5a8c7da7d9e63cfdf6f
Xander Salathe <55741734+xdaybreakerx@users.noreply.github.com>
2026-09-20 11:27:34 +1000

    feat: method scaffold

--- MultiGameApp/NumericalTTTGame.cs
+++ MultiGameApp/NumericalTTTGame.cs
@@ -10,7 +10,8 @@ public class NumericalTTTGame : Game
 
     public int TargetSum { get; }
 
-    public NumericalTTTGame (List<Player> players, Board board, GameMode mode) : base (ValidatePlayers(players), CreateBoardList(board), mode)
+    public NumericalTTTGame(List<Player> players, Board board, GameMode mode)
+        : base(ValidatePlayers(players), CreateBoardList(board), mode)
     {
         _players = players;
         _board = board;
@@ -20,6 +21,75 @@ public class NumericalTTTGame : Game
         // true / false for even / odd
         ValidatePlayerNumbers(_players[0], true, "Player one");
         ValidatePlayerNumbers(_players[1], false, "Player one");
+    }
 
+    private static List<Player> ValidatePlayers(List<Player> players)
+    {
+        //
+    }
+
+    private void ValidatePlayerNumbers(Player player, bool shouldBeOdd, string playerName)
+    {
+        //
+    }
+
+    private static List<Board> CreateBoardList(Board board)
+    {
+        //
+    }
+
+    public override List<Move> GetValidMoves()
+    {
+        //
+    }
+
+    private List<NumberPiece> GetAvailableNumbers(Player player)
+    {
+        //
+    }
+
+    protected override bool IsValidMove(Move move)
+    {
+        //
+    }
+
+    private bool PlayerOwnsNumber(Player player, int value)
+    {
+        //
+    }
+
+    private bool IsNumberUnused(int value)
+    {
+        //
+    }
+
+    public override bool WouldMoveWin(Move move)
+    {
+        //
+    }
+
+    private bool HasWinningLine()
+    {
+        //
+    }
+
+    private bool CheckLineSum(int startingRow, int startingColumn, int rowChange, int columnChange)
+    {
+        //
+    }
+
+    protected override GameResult EvaluateResult()
+    {
+        //
+    }
+
+    private GameResult GetCurrentPlayerWinResult()
+    {
+        //
+    }
+
+    public override string GetHelpText()
+    {
+        //
     }
 }
