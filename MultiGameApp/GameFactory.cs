706bff8ce30ea13beb7f80de1fa3632ec1534368
Xander Salathe <55741734+xdaybreakerx@users.noreply.github.com>
2026-09-21 19:47:50 +1000

    feat(game-factory): create game methods implemented

--- MultiGameApp/GameFactory.cs
+++ MultiGameApp/GameFactory.cs
@@ -17,22 +17,49 @@ public class GameFactory
         };
     }
 
-    private Game CreateNumericalTTTGame(GameMode mode, int boardSize)
+    private static NumericalTTTGame CreateNumericalTTTGame(GameMode mode, int boardSize)
     {
-        throw new NotImplementedException();
+        ArgumentOutOfRangeException.ThrowIfLessThan(boardSize, 3);
+
+        var playerOnePieces = new List<Piece>();
+        var playerTwoPieces = new List<Piece>();
+
+        for (int val = 1; val <= boardSize * boardSize; val++)
+        {
+            var pieces = val % 2 != 0 ? playerOnePieces : playerTwoPieces;
+            pieces.Add(new NumberPiece(val));
+        }
+
+        var players = CreatePlayers(mode, playerOnePieces, playerTwoPieces);
+        return new NumericalTTTGame(players, new Board(boardSize, boardSize), mode);
     }
 
-    private Game CreateNotaktoGame(GameMode mode, int boardSize)
+    private static NotaktoGame CreateNotaktoGame(GameMode mode, int boardSize)
     {
-        throw new NotImplementedException();
+        ValidateBoardSize(boardSize, 3, "Notakto");
+
+        var players = CreatePlayers(mode, [new MarkPiece("X")], [new MarkPiece("X")]);
+        var boards = new List<Board>();
+
+        for (int idx = 0; idx < NotaktoGame.RequiredBoards; idx++)
+        {
+            boards.Add(new Board(NotaktoGame.BoardSize, NotaktoGame.BoardSize));
+        }
+
+        return new NotaktoGame(players, boards, mode);
     }
 
-    private Game CreateGomokuGame(GameMode mode, int boardSize)
+    private static GomokuGame CreateGomokuGame(GameMode mode, int boardSize)
     {
-        throw new NotImplementedException();
+        ValidateBoardSize(boardSize, GomokuGame.BoardSize, "Gomoku");
+
+        var players = CreatePlayers(mode, [new MarkPiece("X")], [new MarkPiece("O")]);
+        var board = new Board(GomokuGame.BoardSize, GomokuGame.BoardSize);
+
+        return new GomokuGame(players, board, mode);
     }
 
-    private List<Player> CreatePlayers(
+    private static List<Player> CreatePlayers(
         GameMode mode,
         List<Piece> playerOnePieces,
         List<Piece> playerTwoPieces
@@ -46,7 +73,7 @@ public class GameFactory
         return [new HumanPlayer(1, "Player 1", playerOnePieces), playerTwo];
     }
 
-    private void ValidateBoardSize(int boardSize, int requiredSize, string gameName)
+    private static void ValidateBoardSize(int boardSize, int requiredSize, string gameName)
     {
         if (boardSize != requiredSize)
             throw new ArgumentOutOfRangeException(
